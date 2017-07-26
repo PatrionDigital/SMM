ec792556e63758d2891dbdf50400edc8bf888b84
Davis Patrick <davipatr@jp.co.square-enix.com>
2017-07-26 18:11:05 +0900

    - first commit with files from the Book

---
+++ authenticate.R
@@ -0,0 +1,17 @@
+# install.packages(c("devtools", "rjson", "bit64", "httr"))
+# #RESTART R session!
+# library(devtools)
+# install_github("twitteR", username="geoffjentry")
+
+require(twitteR)
+api_key <- "LcOPPlyMAV3u8Ymy0wlvBEzxO"
+api_secret <- "rrvUxwEandPEiBHPUloWQqgtz6L4NS0MhubMaBAFsWA08tC0nR"
+access_token <- "8130922-UxTjJkB8ZDOOD4paanzUJtWjuFMOrnMKbfBEE2akTz"
+access_token_secret <- "WvEQsYTahlKAbXp02bpO2hFlBaIhedwbKDsyzFsITm6x2"
+
+setup_twitter_oauth(api_key,api_secret,access_token,access_token_secret)
+rm(api_key)
+rm(api_secret)
+rm(access_token)
+rm(access_token_secret)
+# EQ = searchTwitter("EarthQuake", since='2014-09-24')
