d30cc3b742b1dd7be98f6b95a1a4be595e7d4f8b
Patrick S. Davis <info@patriondigital.com>
2017-08-04 17:34:13 +0900

    - get and preprocess tweets for Vec2Doc mining method

---
+++ getTweetsDoc2Vec.R
@@ -0,0 +1,30 @@
+## Fetching Tweets ##
+download.file(url = "http://curl.haxx.se/ca/cacert.pem",
+  destfile = "cacert.pem")
+source('authenticate.R')
+
+df_tweets <- twListToDF(searchTwitter('siggraph OR #siggraph', n = 2500, lang = 'en')) %>%
+  #converting some symbols (emoji, etc)
+  dmap_at('text', conv_fun)
+
+# preprocessing and tokenization
+it_tweets <- itoken(df_tweets$text,
+  preprocessor = prep_fun,
+  tokenizer = tok_fun,
+  ids = df_tweets$id,
+  progressbar = TRUE)
+
+# create the vocabulary and document-term matrix
+dtm_tweets <- create_dtm(it_tweets, vectorizer)
+
+# transforming data with tf-idf
+dtm_tweets_tfidf <- fit_transform(dtm_tweets, tfidf)
+
+# loading classification model
+glmnet_classifier <- readRDS('glmnet_classifier.RDS')
+
+# predict probabilities of positiveness
+preds_tweets <- predict(glmnet_classifier, dtm_tweets_tfidf, type = 'response')[ ,1]
+
+# adding predicted probabilities to the initital dataset
+df_tweets$sentiment <- preds_tweets
